de1129c2f1c890906a0f4a42214db3058af3f1b7
Juliano Ribeiro de Souza Maciel <juliano.maciel@faculdadefacec.edu.br>
2022-09-01 19:08:20 -0300

    Implementações aula

---
+++ Tipos.String.Char/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Tipos.String.Char
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Digite seu nome:");
+                string nome = Console.ReadLine();
+                string nomeValidado = nome.Trim();
+
+                if (string.IsNullOrWhiteSpace(nomeValidado))
+                    throw new Exception("Nome inválido.");
+
+                Console.WriteLine("Primeira letra do meu nome:" + nomeValidado[0]);
+                Console.WriteLine("Ultima letra do meu nome:" + nomeValidado[nomeValidado.Length -1]);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
+    }
+}
